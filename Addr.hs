986c435f025fdbbc8cae68e222468fc7b3de8b36
Andres Erbsen <andres.erbsen@gmail.com>
2012-10-09 08:17:33 +0300

    Rewrite in C++ as lingua franca or something

--- Addr.hs
+++
@@ -1,9 +0,0 @@
-import Data.Serialize
-import Data.Word
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as B
-
--- VindiCat + Address = Caddress
-type Caddress = Integer
-
--- I do not know what else to write here, so I'll just wait until I get to talk to someone smarter than me.
