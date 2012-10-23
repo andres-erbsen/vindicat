20eaedf8494996422b58fcacf2426ff3d58c2527
Andres Erbsen <andres.erbsen@gmail.com>
2012-10-24 01:38:34 +0300

    hopefully fix build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 echo g++ --std=c++11 -O3 \
-	"$1" -o "${1%.*}" \
+	-o "${1%.*}" \
 	-DEV_STANDALONE          \
 	lib/libev/ev.c           \
 	lib/libsocket/*.c*       \
 	-I lib/libsocket -I lib/libev $@
 g++ --std=c++11 -O3 \
-	"$1" -o "${1%.*}" \
+	-o "${1%.*}" \
 	-DEV_STANDALONE          \
 	lib/libev/ev.c           \
 	lib/libsocket/*.c*       \
