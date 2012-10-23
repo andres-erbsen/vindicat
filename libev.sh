1162c4fc21f0442111c99793af7164bee7deccdd
Andres Erbsen <andres.erbsen@gmail.com>
2012-10-24 00:33:51 +0300

    Use latest libev, not github mirror

--- libev.sh
+++ libev.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+LIBEV=libev-4.11
 
 mkdir -p lib/libev || :
 cd lib/libev
+wget "http://dist.schmorp.de/libev/Attic/$LIBEV.tar.gz"
 
-for file in ev.h ev.c ev++.h ev_vars.h ev_wrap.h ev_select.c ev_poll.c ev_epoll.c;
-do
-	rm "$file" | :
-	wget "https://raw.github.com/brimworks/libev/master/$file"
-done
+tar -xzf "./$LIBEV.tar.gz" 
+cp -t . "./$LIBEV/ev.h" "./$LIBEV/ev.c" "./$LIBEV/ev++.h" "./$LIBEV/ev_vars.h" "./$LIBEV/ev_wrap.h" "./$LIBEV/ev_select.c" "./$LIBEV/ev_poll.c" "./$LIBEV/ev_epoll.c"
+
+rm "$LIBEV.tar.gz"
+rm -rf "$LIBEV"
