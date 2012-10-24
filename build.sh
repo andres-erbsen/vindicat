7e8f5572e5ebaa30b10f7a14103962935c8f0639
Andres Erbsen <andres.erbsen@gmail.com>
2012-10-24 09:52:21 +0300

    no auto-optimize in build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-echo g++ --std=c++11 -O3 \
+echo g++ --std=c++11 \
 	-o "${1%.*}" \
 	-DEV_STANDALONE          \
 	lib/libev/ev.c           \
 	lib/libsocket/*.c*       \
 	-I lib/libsocket -I lib/libev $@
-g++ --std=c++11 -O3 \
+g++ --std=c++11 \
 	-o "${1%.*}" \
 	-DEV_STANDALONE          \
 	lib/libev/ev.c           \
