531fa550380e16b5aae2a9136de24ada28c9085e
Andres Erbsen <andres.erbsen@gmail.com>
2013-02-01 11:58:29 +0200

    include a copy of the obscurer libraries for developer convenience

---
+++ lib/libsocket/dgramclient.cpp
@@ -0,0 +1,137 @@
+# include <string>
+# include <unistd.h>
+# include <string.h>
+
+/*
+The committers of the libsocket project, all rights reserved
+(c) 2012, dermesser <lbo@spheniscida.de>
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+	disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+	disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*
+ * DESCRIPTION FOR DGRAMCLIENT.CPP
+ * 	dgramclient.cpp contains the class dgram_client_socket. This
+ * 	class provides the read/write functions for connected datagram
+ * 	sockets (alias client dgram sockets...). This helps to avoid
+ * 	writing the same code more than one time.
+ * 	The client datagram classes for specific domains which are
+ * 	unix_dgram_client and inet_dgram_client inherit the functions
+ * 	declared here to get the ability to send and receive
+ * 	data via connected datagram sockets. It is not possible to use
+ * 	a class which has this functions also for stream sockets because
+ * 	the functions have to check if the socket is connected (bool connected).
+ * 	In the stream classes, this check is realized by checking if the file
+ * 	descriptor is -1 or not.
+*/
+
+# include "exception.hpp"
+# include "dgramclient.hpp"
+
+namespace libsocket
+{
+	using std::string;
+
+	dgram_client_socket::dgram_client_socket(void)
+		: connected(false)
+	{}
+
+	ssize_t dgram_client_socket::rcv(void* buf, size_t len, int flags)
+	{
+		ssize_t bytes;
+
+		if ( connected != true )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket::rcv() - Socket is not connected!\n");
+
+		memset(buf,0,len);
+
+		if ( -1 == (bytes = recv(sfd,buf,len,flags)) )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket::rcv() - recv() failed!\n");
+
+		return bytes;
+	}
+
+	dgram_client_socket& operator>>(dgram_client_socket& sock, string& dest)
+	{
+		ssize_t read_bytes;
+
+		if ( sock.connected != true )
+			throw socket_exception(__FILE__,__LINE__,">>(dgram_client_socket, std::string) - Socket is not connected!\n");
+
+		char* buffer = new char[dest.size()];
+
+		memset(buffer,0,dest.size());
+
+		if ( -1 == (read_bytes = read(sock.sfd,buffer,dest.size())) )
+			throw socket_exception(__FILE__,__LINE__,">>(dgram_client_socket, std::string) input: Error while reading!\n");
+
+		if ( read_bytes < static_cast<ssize_t>(dest.size()) )
+			dest.resize(read_bytes); // So the client doesn't print content more than one time
+						 // and it can check if the string's length is 0 (end of transmission)
+
+		dest.assign(buffer,read_bytes);
+
+		delete[] buffer;
+
+		return sock;
+	}
+
+	ssize_t dgram_client_socket::snd(const void* buf, size_t len, int flags)
+	{
+		ssize_t bytes;
+
+		if ( connected != true )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket::snd() - Socket is not connected!\n");
+
+		if ( -1 == (bytes = send(sfd,buf,len,flags)) )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket::snd() - send() failed!\n");
+
+		return bytes;
+	}
+
+	dgram_client_socket& operator<<(dgram_client_socket& sock, const char* str)
+	{
+		if ( str == NULL )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket <<(const char*) output: Null buffer given!\n");
+		if ( sock.connected == false )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket <<(const char*) output: DGRAM socket not connected!\n");
+
+		size_t len = strlen(str);
+
+		if ( -1 == write(sock.sfd,str,len) )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket <<(const char*) output: Write failed!\n");
+
+		return sock;
+	}
+
+	dgram_client_socket& operator<<(dgram_client_socket& sock, string& str)
+	{
+		if ( sock.connected == false )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket <<(std::string) output: DGRAM socket not connected!\n");
+		if ( -1 == write(sock.sfd,str.c_str(),str.size()) )
+			throw socket_exception(__FILE__,__LINE__,"dgram_client_socket <<(std::string) output: Write failed!\n");
+
+		return sock;
+	}
+
+	// Getters
+	bool dgram_client_socket::getconn(void) const
+	{
+		return connected;
+	}
+}
---
+++ lib/libsocket/inetdgram.cpp
@@ -0,0 +1,143 @@
+# include <iostream>
+# include <string>
+# include <string.h>
+
+# include <unistd.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <stdio.h>
+
+// oo wrapper around libinetsocket
+/*
+The committers of the libsocket project, all rights reserved
+(c) 2012, dermesser <lbo@spheniscida.de>
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+	disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+	disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*
+ * DESCRIPTION FOR INETDGRAM.CPP
+ * 	The class inet_dgram provides that functions which
+ * 	are used for all UDP sockets: rcvfrom and sndto. These
+ * 	may be used for both client and server UDP sockets.
+ */
+
+# include "libinetsocket.h"
+# include "exception.hpp"
+# include "inetdgram.hpp"
+
+namespace libsocket
+{
+	using std::string;
+
+	// I/O
+
+	// I
+
+	ssize_t inet_dgram::rcvfrom(void* buf, size_t len, char* hostbuf, size_t hostbuflen, char* portbuf, size_t portbuflen, int rcvfrom_flags, bool numeric)
+	{
+		ssize_t bytes;
+		int num = ((numeric == true) ? LIBSOCKET_NUMERIC : 0);
+
+		if ( -1 == sfd )
+			throw socket_exception(__FILE__,__LINE__,"inet_dgram::rcvfrom() - Socket already closed!\n");
+
+		if ( -1 == (bytes = recvfrom_inet_dgram_socket(sfd,buf,len,hostbuf,hostbuflen,portbuf,portbuflen,rcvfrom_flags,num)) )
+			throw socket_exception(__FILE__,__LINE__,"inet_dgram::rcvfrom() - recvfrom() failed!\n");
+
+		return bytes;
+	}
+
+
+	ssize_t inet_dgram::rcvfrom(void* buf, size_t len, string& srchost, string& srcport, int rcvfrom_flags, bool numeric)
+	{
+		ssize_t bytes;
+
+		char* from_host = new char[1024]; // Let's say, that's enough
+		char* from_port = new char[32];
+
+		memset(from_host,0,1024);
+		memset(from_port,0,32);
+		memset(buf,0,len);
+
+		bytes = rcvfrom(buf,len,from_host,1023,from_port,31,rcvfrom_flags,numeric);
+
+		srchost.resize(strlen(from_host));
+		srcport.resize(strlen(from_port));
+
+		srchost.assign(from_host);
+		srcport.assign(from_port);
+
+		delete[] from_host;
+		delete[] from_port;
+
+		return bytes;
+	}
+
+	ssize_t inet_dgram::rcvfrom(string& buf, string& srchost, string& srcport, int rcvfrom_flags, bool numeric)
+	{
+		ssize_t bytes;
+
+		char* cbuf = new char[buf.size()];
+
+		memset(cbuf,0,buf.size());
+
+		bytes = rcvfrom(cbuf,static_cast<size_t>(buf.size()),srchost,srcport,rcvfrom_flags,numeric); // calling inet_dgram::rcvfrom(void*, size_t, string&, string&, int, bool)
+
+		buf.resize(bytes);
+
+		buf.assign(cbuf,bytes);
+
+		delete[] cbuf;
+
+		return bytes;
+	}
+
+	// O
+
+	ssize_t inet_dgram::sndto(const void* buf, size_t len, const char* dsthost, const char* dstport, int sndto_flags)
+	{
+		ssize_t bytes;
+
+		if ( -1 == sfd )
+			throw socket_exception(__FILE__,__LINE__,"inet_dgram::sendto() - Socket already closed!\n");
+
+		if ( -1 == (bytes = sendto_inet_dgram_socket(sfd,buf,len,dsthost,dstport,sndto_flags)) )
+			throw socket_exception(__FILE__,__LINE__,"inet_dgram::sndto() - Error at sendto\n");
+
+		return bytes;
+	}
+
+	ssize_t inet_dgram::sndto(const void* buf, size_t len, const string& dsthost, const string& dstport, int sndto_flags)
+	{
+		ssize_t bytes;
+
+		bytes = sndto(buf,len,dsthost.c_str(),dstport.c_str(),sndto_flags);
+
+		return bytes;
+	}
+
+	ssize_t inet_dgram::sndto(const string& buf, const string& dsthost, const string& dstport, int sndto_flags)
+	{
+		ssize_t bytes;
+
+		bytes = sndto(buf.c_str(),buf.size(),dsthost.c_str(),dstport.c_str(),sndto_flags);
+
+		return bytes;
+	}
+}
