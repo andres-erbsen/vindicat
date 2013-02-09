7ac17d86a0dfd6bcc74ddbba923a56ac1c341272
Andres Erbsen <andres.erbsen@gmail.com>
2013-02-09 16:04:13 +0200

    ConnectionPool is just a map

--- src/ConnectionPool.h
+++ src/ConnectionPool.h
@@ -1,7 +1,11 @@
 #ifndef CONNECTIONPOOL_H_
 #define CONNECTIONPOOL_H_
 
-class ConnectionPool {
-};
+#include "Connection.h"
+#include "PrefixMap.h"
+#include <memory>
+
+typedef PrefixMap< std::shared_ptr<Connection> > ConnectionPool;
+// maps device identifiers to our connections to them
 
 #endif // CONNECTIONPOOL_H_
