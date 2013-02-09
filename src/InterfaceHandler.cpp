384b1598ccac521be1050a667f58a5d45e4d3808
Andres Erbsen <andres.erbsen@gmail.com>
2013-02-09 16:05:31 +0200

    InterfaceHandler.cpp: variable name

--- src/InterfaceHandler.cpp
+++ src/InterfaceHandler.cpp
@@ -1,11 +1,13 @@
 #include "InterfaceHandler.h"
 #include <cassert>
+#include <utility>
+
 InterfaceHandler::InterfaceHandler(NetworkMap& nm, ConnectionPool& cp)
 	: _nm(nm)
 	, _cp(cp)
 	{}
 
-void InterfaceHandler::operator()(std::string&& from, std::string&& packet)
+void InterfaceHandler::operator()(std::string&& to, std::string&& packet)
 {
 	assert(0);
 }
