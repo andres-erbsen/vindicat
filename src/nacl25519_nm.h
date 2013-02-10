0d72f06b1c099c8045affa88737ab645ec6975cf
Andres Erbsen <andres.erbsen@gmail.com>
2013-02-10 10:47:30 +0200

    Access our pk from nacl precomputation

--- src/nacl25519_nm.h
+++ src/nacl25519_nm.h
@@ -14,6 +14,7 @@ public:
 	nacl25519_nm(const std::string& pk, const std::string& sk);
 
 	void pk(const std::string& pk);
+	std::string our_pk();
 
 	std::string encrypt(const std::string&, const std::string&) const;
 	bool decrypt(const std::string&, const std::string&, std::string&) const;
@@ -22,6 +23,7 @@ private:
 	unsigned char _k[crypto_box_BEFORENMBYTES];
 	unsigned char _pk[crypto_box_PUBLICKEYBYTES];
 	unsigned char _sk[crypto_box_SECRETKEYBYTES];
+	unsigned char _our_pk[crypto_box_PUBLICKEYBYTES];
 };
 
 
