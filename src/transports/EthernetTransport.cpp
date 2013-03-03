b1937e59c6ac379ad8dbb475e8a0b535deea4ca2
Andreas Ots <andreasots@gmail.com>
2013-03-03 19:21:38 +0200

    EthernetTransport: now works on systems where pcap_selectable_fd() returns -1

--- src/transports/EthernetTransport.cpp
+++ src/transports/EthernetTransport.cpp
@@ -7,8 +7,9 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <functional>
 
-EthernetTransport::EthernetTransport(const std::string& d)
+EthernetTransport::EthernetTransport(const std::string& d): _fd{-1, -1}
 {
   char errbuf[PCAP_ERRBUF_SIZE];
   const char *device;
@@ -63,19 +64,49 @@ EthernetTransport::EthernetTransport(const std::string& d)
     pcap_perror(_pcap, "pcap_setfilter");
     std::abort();
   }
+
+  _fd[0] = pcap_get_selectable_fd(_pcap);
+  if(_fd[0] == -1 && pipe(_fd) == -1)
+  {
+    std::perror("pipe");
+    std::abort();
+  }
+}
+
+EthernetTransport::~EthernetTransport()
+{
+  pcap_breakloop(_pcap);
+  if(_pcap_loop_thread.joinable())
+    _pcap_loop_thread.join();
+  pcap_close(_pcap);
+  if(_fd[1] != -1)
+  {
+    close(_fd[0]);
+    close(_fd[1]);
+  }
 }
 
 void EthernetTransport::enable()
 {
   _read_watcher.set<EthernetTransport, &EthernetTransport::read_cb>(this);
-  int fd = pcap_get_selectable_fd(_pcap);
-  // pcap_get_selectable_fd() returns, on UNIX, a file descriptor number for a
-  // file descriptor on which one can do a select() or poll() to wait for it to
-  // be possible to read packets without blocking, if such a descriptor exists,
-  // or -1, if no such descriptor exists. pcap_get_selectable_fd() is not
-  // available on Windows.
-  assert(fd != -1);
-  _read_watcher.start(fd, ev::READ);
+  _read_watcher.start(_fd[0], ev::READ);
+  if(_fd[1] != -1)
+    _pcap_loop_thread = std::thread(std::mem_fn(&EthernetTransport::pcap_loop_thread), this);
+}
+
+void EthernetTransport::pcap_loop_thread()
+{
+  pcap_loop(_pcap, -1, &EthernetTransport::pcap_callback,
+            reinterpret_cast<std::uint8_t*>(_fd));
+}
+
+void EthernetTransport::pcap_callback(std::uint8_t *data,
+                                      const pcap_pkthdr* pcap_header,
+                                      const std::uint8_t* packet)
+{
+  int *fd = reinterpret_cast<int*>(data);
+  write(fd[1], pcap_header, sizeof(pcap_pkthdr));
+  write(fd[1], packet, pcap_header->caplen);
 }
 
 void EthernetTransport::broadcast(const std::string& msg)
@@ -100,11 +131,22 @@ void EthernetTransport::read_cb(ev::io &watcher, int revents)
 {
   const std::uint8_t *packet;
   pcap_pkthdr *header;
-  int res = pcap_next_ex(_pcap, &header, &packet);
-  if(res == -1)
-    pcap_perror(_pcap, "pcap_next_ex");
-  else if(res != 1)
-    return;
+  if(_fd[1] == -1)
+  {
+    int res = pcap_next_ex(_pcap, &header, &packet);
+    if(res == -1)
+      pcap_perror(_pcap, "pcap_next_ex");
+    if(res != 1)
+      return;
+  }
+  else
+  {
+    header = new pcap_pkthdr;
+    read(_fd[0], header, sizeof(pcap_pkthdr));
+    packet = new std::uint8_t[header->caplen];
+    read(_fd[0], const_cast<std::uint8_t*>(packet), header->caplen);
+  }
+
   const ether_header *eth = reinterpret_cast<const ether_header*>(packet);
   const std::string from_mac( reinterpret_cast<const char*>(eth->ether_shost)
                             , ETH_ALEN );
@@ -116,4 +158,10 @@ void EthernetTransport::read_cb(ev::io &watcher, int revents)
                                          , from_mac )
                               , "ETHER"+from_mac )
              , std::move(payload) );
+
+  if(_fd[1] != -1)
+  {
+    delete header;
+    delete[] packet;
+  }
 }
