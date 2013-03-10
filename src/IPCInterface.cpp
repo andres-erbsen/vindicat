98c3f1973aad0515fd1c797d3f656d4c488e58dd
Andreas Ots <andreasots@gmail.com>
2013-03-10 18:06:13 +0200

    Server interface complete

--- src/IPCInterface.cpp
+++ src/IPCInterface.cpp
@@ -4,13 +4,14 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <netinet/in.h>
+#include "libvindicat.pb.h"
 
 #ifndef UNIX_PATH_MAX
 #define UNIX_PATH_MAX sizeof(sockaddr_un::sun_path)
 #endif
 
 IPCInterface::IPCInterface(const std::string& id)
-    : _ip(('\x04'+id).substr(0, 16)) {
+    : _id(id) {
   if((_fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
     std::perror("socket");
     std::abort();
@@ -34,18 +35,11 @@ bool IPCInterface::match(const std::string& from, std::uint8_t next_header,
   std::uint16_t port = reinterpret_cast<const std::uint16_t*>(data.data())[1];
   switch(next_header) {
     case IPPROTO_TCP:
-      return _tcp.count(from) > 0 && _tcp[from].count(port) > 0;
+      return _tcp.count(port) > 0;
     case IPPROTO_UDP:
-      return _udp.count(from) > 0 && _udp[from].count(port) > 0;
+      return _udp.count(port) > 0;
     default:
-      if(_clients.count(from) > 0) {
-        auto range = _clients.equal_range(from);
-	range.second++;
-        for(auto i = range.first; i != range.second; i++)
-	  if(i->first == from)
-	    return true;
-      }
-      return false;
+      return _clients.count(next_header) > 0;
   }
 }
 
@@ -55,59 +49,122 @@ void IPCInterface::send(const std::string& from, std::uint8_t next_header,
   std::memset(&client, 0, sizeof(client));
   client.sun_family = AF_UNIX;
   std::uint16_t port = reinterpret_cast<const std::uint16_t*>(data.data())[1];
+  libvindicat::Packet packet;
+  packet.set_identificator(from);
+  packet.set_next_header(next_header);
+  packet.set_payload(data);
+  std::string message("\x02", 1);
+  packet.AppendToString(&message);
+
   switch(next_header) {
     case IPPROTO_TCP:
-      std::memcpy(client.sun_path, _tcp[from][port].data(),
-                  _tcp[from][port].size());
-      send(client, from, next_header, data);
-      return;
-    case IPPROTO_UDP:
-      std::memcpy(client.sun_path, _udp[from][port].data(),
-                  _udp[from][port].size());
-      send(client, from, next_header, data);
-      return;
-    default:
-      auto range = _clients.equal_range(from);
-      range.second++;
-      for(auto i = range.first; i != range.second; i++) {
+      for(auto i = _tcp.find(port); i != _tcp.end() && i->first == port; i++) {
         std::memset(client.sun_path, 0, UNIX_PATH_MAX);
-        std::memcpy(client.sun_path, i->second.data(),
-                    i->second.size());
-	send(client, from, next_header, data);
+	std::memcpy(client.sun_path, i->second.data(), i->second.size());
+        send(client, message);
+      }
+      break;
+    case IPPROTO_UDP:
+      for(auto i = _udp.find(port); i != _udp.end() && i->first == port; i++) {
+        std::memset(client.sun_path, 0, UNIX_PATH_MAX);
+	std::memcpy(client.sun_path, i->second.data(), i->second.size());
+	send(client, message);
+      }
+      break;
+    default:
+      for(auto i = _clients.find(next_header);
+          i != _clients.end() && i->first == next_header; i++) {
+        std::memset(client.sun_path, 0, UNIX_PATH_MAX);
+        std::memcpy(client.sun_path, i->second.data(), i->second.size());
+        send(client, message);
       }
   }
 }
-void IPCInterface::send(const sockaddr_un &client, const std::string& from,
-                        std::uint8_t next_header, const std::string& data) {
-  auto res = sendto(_fd, data.data(), data.size(), 0,
+
+void IPCInterface::send(const sockaddr_un &client, const std::string& msg) {
+  auto res = sendto(_fd, msg.data(), msg.size(), 0,
                     reinterpret_cast<const sockaddr*>(&client),
                     sizeof(client));
+
   if(res == -1)
     clear(client);
 }
 
 void IPCInterface::clear(const sockaddr_un& client) {
   std::string client_path = std::string(client.sun_path, UNIX_PATH_MAX);
-  for(auto proto : {_tcp, _udp})
-    for(auto server = proto.begin(); server != proto.end();) {
-      for(auto conn = server->second.begin(); conn != server->second.end();) {
-        if(conn->second == client_path) {
-          conn = server->second.erase(conn);
-	  continue;
-        }
+  for(auto proto : {&_tcp, &_udp})
+    for(auto conn = proto->begin(); conn != proto->end();)
+      if(conn->second == client_path)
+        conn = proto->erase(conn);
+      else
         conn++;
-      }
-      if(server->second.empty()) {
-        server = proto.erase(server);
-	continue;
-      }
-      server++;
-    }
-  for(auto conn = _clients.begin(); conn != _clients.end();) {
-    if(conn->second == client_path) {
+  for(auto conn = _clients.begin(); conn != _clients.end();)
+    if(conn->second == client_path)
       conn = _clients.erase(conn);
-      continue;
-    }
-    conn++;
+    else
+      conn++;
+}
+
+void IPCInterface::read_cb(ev::io&, int) {
+  sockaddr_un from;
+  socklen_t length = sizeof(from);
+  std::size_t buflen = 0, buflen_len = sizeof(buflen);
+
+  if(getsockopt(_fd, SOL_PACKET, SO_RCVBUF, &buflen, &buflen_len) == -1) {
+    std::perror("getsockopt(SO_RCVBUF)");
+    std::abort();
   }
+  assert(buflen_len == sizeof(buflen));
+
+  char *buf = new char[buflen];
+  auto buf_len = recvfrom(_fd, buf, buflen, 0,
+                          reinterpret_cast<sockaddr*>(&from), &length);
+  if(buf_len >= 1) {
+    switch(buf[0]) {
+      case 0x00: {  // Configuration request
+        libvindicat::ConfigurationResponse response;
+        response.set_local_identifier(_id);
+        std::string message("\x00", 1);
+        response.AppendToString(&message);
+        send(from, message);
+	break;
+      }
+      case 0x01: {  // Forward request
+        std::string client_path(from.sun_path, UNIX_PATH_MAX); 
+        libvindicat::ForwardRequest request;
+	if(!request.ParseFromArray(buf, buf_len) ||
+	    request.next_header() > 256 ||
+            (request.next_header() == IPPROTO_TCP &&
+	       !(request.has_tcp() && request.tcp().port() > 0 &&
+                   request.tcp().port() <= 0xFFFF)) ||
+	    (request.next_header() == IPPROTO_UDP &&
+               !(request.has_udp() && request.udp().port() > 0 &&
+                   request.udp().port() <= 0xFFFF ))) {
+	  send(from, "\x01");
+	  break;
+	}
+	switch(request.next_header()) {
+          case IPPROTO_TCP:
+	    _tcp.insert(std::make_pair(request.tcp().port(), client_path));
+	    break;
+	  case IPPROTO_UDP:
+	    _udp.insert(std::make_pair(request.udp().port(), client_path));
+	  default:
+	    _clients.insert(std::make_pair(request.next_header(),
+                                           client_path));
+	}
+	break;
+      }
+      case 0x02: {  // Packet
+        libvindicat::Packet packet;
+	if(packet.ParseFromArray(buf, buf_len)) {
+	  _receive_cb(std::string(packet.identificator()),
+              static_cast<char>(packet.next_header())+packet.payload());
+	}
+	break;
+      }
+    }
+  }
+
+  delete[] buf;
 }
--- src/IPCInterface.h
+++ src/IPCInterface.h
@@ -3,9 +3,11 @@
 
 #include "Interface.h"
 #include <string>
+
 #include <unordered_map>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <ev++.h>
 
 /// Use Inter-process communication to give local applications access to
 /// the network.
@@ -19,19 +21,20 @@ class IPCInterface: public Interface {
   virtual bool match(const std::string&, std::uint8_t, const std::string&);
   virtual void send(const std::string&, std::uint8_t, const std::string&);
  private:
-  std::string _ip;
+  std::string _id;
 
   // Unix domain socket
   sockaddr_un _addr;
   int _fd;
-  // Remote to local mapping
-  std::unordered_map<std::string,
-                     std::unordered_map<std::uint16_t, std::string>> _tcp,
-                                                                     _udp;
-  std::unordered_multimap<std::string, std::string> _clients;
+  friend ev::io;
+  ev::io _read_watcher;
+
+  std::unordered_multimap<std::uint16_t, std::string> _tcp, _udp;
+  std::unordered_multimap<std::uint8_t, std::string> _clients;
+
   void clear(const sockaddr_un&);
-  void send(const sockaddr_un&, const std::string&, std::uint8_t,
-            const std::string&);
+  void send(const sockaddr_un&, const std::string&);
+  void read_cb(ev::io&, int);
 };
 
 #endif
